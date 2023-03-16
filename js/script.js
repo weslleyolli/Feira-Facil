5cc3de9a36c6d9eca8ac1affd28e9118c8874c69
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-15 23:12:57 -0300

    function validForms added in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert('Hello to feira facil')
+const buttonSend = document.getElementById('enter')
+
+buttonSend.addEventListener('click', (e) => {
+    e.preventDefault()
+    validarFormulario()
+})
+
+function validarFormulario() {
+    const userName = document.getElementById("username");
+    const password = document.getElementById("password");
+    console.log(userName)
+  
+    if (userName === null || password === null) {
+      alert("Por favor, preencha todos os campos.");
+      return false;
+    }
+}

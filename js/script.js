8764b68f4da43a7981a0ad166a56b92e11b419b4
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-17 22:32:46 -0300

    js in popover added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 const buttonSend = document.getElementById('enter')
 
+
 buttonSend.addEventListener('click', (e) => {
-    e.preventDefault()
-    validarFormulario()
+  e.preventDefault()
+  validarFormulario()
 })
 
 function validarFormulario() {
-    const userName = document.getElementById("username");
-    const password = document.getElementById("password");
-    console.log(userName)
-  
-    if (userName === null || password === null) {
-      alert("Por favor, preencha todos os campos.");
-      return false;
-    }
+  const userName = document.getElementById("username");
+  const password = document.getElementById("password");
+  console.log(userName)
+
+  if (userName === null || password === null) {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+  }
 }
+
+
+

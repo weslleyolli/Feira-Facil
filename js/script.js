c12c0ea4e332c50d293d6e93e0aad112c245839b
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-20 13:57:32 -0300

    added prevent default

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const buttonSend = document.getElementById('enter');
 
 buttonSend.addEventListener('click', (e) => {
+  function validarFormulario() {
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    console.log(userName)
+    console.log(password)
+    if (userName == '' || password == '') {
+
+      alert("Por favor, preencha todos os campos.");
+      e.preventDefault()
+      return false;
+    }
+  }
   validarFormulario();
 });
 
-function validarFormulario() {
-  let userName = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  console.log(userName)
-  console.log(password)
-  if (userName == '' || password == '') {
-    alert("Por favor, preencha todos os campos.");
-    return false;
-  }
-}
 
 
 
